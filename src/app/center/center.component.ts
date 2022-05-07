54f39188746a5e8b92f6999599e80c61c92c9c59
NAGARJUN <dasnagarjun1994@gmail.com>
2022-05-07 13:16:06 +0530

    bhai au tk edit haba on edit ta

--- src/app/center/center.component.ts
+++ src/app/center/center.component.ts
@@ -1,41 +1,53 @@
 import { Component, OnInit } from '@angular/core';
-import { Todo } from './todo'
-import { TodoService } from '../todo.service'
+import { Todo } from './todo';
+import { TodoService } from '../todo.service';
+import { FormBuilder } from '@angular/forms';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-center',
   templateUrl: './center.component.html',
-  styleUrls: ['./center.component.css']
+  styleUrls: ['./center.component.css'],
 })
 export class CenterComponent implements OnInit {
   todos: Todo[];
-  constructor(private todoservice:TodoService) {
-  }
+  constructor(private todoservice: TodoService, private fb: FormBuilder) {}
+
+  profileForm = this.fb.group({
+    title: ['', Validators.required],
+    releaseDate: ['', Validators.required],
+  });
   ngOnInit() {
-    this.todoservice.getAllTodos()
-      .subscribe((response: Todo[]) => {
-        this.todos = response;
-      });
+    this.todoservice.getAllTodos().subscribe((response: Todo[]) => {
+      this.todos = response;
+    });
   }
 
-  deleteData(id:any){
-    this.todoservice.deleteTodos(id).subscribe((deleteResponse)=>{
+  get registerFormControl() {
+    return this.profileForm.controls;
+  }
+
+  deleteData(id: any) {
+    this.todoservice.deleteTodos(id).subscribe((deleteResponse) => {
       console.log(deleteResponse);
       this.ngOnInit();
-    })
+    });
   }
 
-
-  updatefun(id: any){
-
+  updatefun(id: any) {
     console.log(id);
-    this.todoservice.getuserbyId(id).subscribe((data)=>{
+    this.todoservice.getuserbyId(id).subscribe((data: any) => {
       console.log(data);
-     
-    })
-
-
+      this.profileForm.controls['title'].setValue(data.title);
+      this.profileForm.controls['releaseDate'].setValue(data.releaseDate);
+    });
   }
 
+  onEdit(data) {
+    console.log(data);
+    console.log("on edit method is called");
+   
+    this.profileForm.reset();
+    document.getElementById("clo").click();
+  }
 }
-

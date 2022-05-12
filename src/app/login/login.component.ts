bd0262408464ac38a8ce95baebf405771eb55300
sameer samantra <sanjaysamantra1@gmail.com>
2022-05-12 16:01:06 +0530

    login jwt token working

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -13,12 +15,18 @@ export class LoginComponent implements OnInit {
     password: ['', Validators.required],
   });
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder, private http: HttpClient, private router:Router) {}
 
   ngOnInit(): void {}
 
   onLogin(x){
     console.log(this.loginForm.getRawValue());
+    this.http.post('http://localhost:3000/login',x,{withCredentials: true}).subscribe((logindata)=>{
+      console.log("hiiii" + logindata);
+      this.router.navigate(['/']);
+    
+    })
     console.log('login works')
+
   }
 }
